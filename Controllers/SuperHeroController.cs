2121aa7e014eb00f59080deed217acd2e04c27fe
Navid Yazdani <mohammadYazdani7@gmail.com>
2022-06-28 13:20:37 -0700

    Api fixed

---
+++ Controllers/SuperHeroController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+namespace SuperHero_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
+        {
+            return new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Name = "Spider Man",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                }
+            };
+        }
+    }
+}
+
---
+++ SuperHero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SuperHero_API
+{
+	public class SuperHero
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; } = String.Empty;
+
+        public string FirstName { get; set; } = String.Empty;
+
+        public string LastName { get; set; } = String.Empty;
+
+        public string Place { get; set; } = String.Empty;
+    }
+}
+
